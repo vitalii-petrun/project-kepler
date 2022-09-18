5b130f3b1c0f775148e9d6bff13ae0f535e1f4d5
vitalii-petrun <vitalii.petrun@gmail.com>
2022-09-18 22:03:02 +0300

    Update home_page_cubit.dart

--- lib/presentation/blocs/home_page/home_page_cubit.dart
+++ lib/presentation/blocs/home_page/home_page_cubit.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:project_kepler/domain/repositories/api_repository.dart';
+import 'package:project_kepler/data/repositories/api_repository_impl.dart';
 import 'package:project_kepler/presentation/blocs/home_page/home_page_state.dart';
 
 class HomePageCubit extends Cubit<HomePageState> {
-  final ApiRepository repository;
+  final ApiRepositoryImpl repository;
 
   HomePageCubit(this.repository) : super(LaunchesInit());
   void fetch() async {
