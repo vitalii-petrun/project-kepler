5844f275ebb87510b8cb74a3622abc2f7de331f2
vitalii-petrun <vitalii.petrun@gmail.com>
2022-09-18 21:58:50 +0300

    cubit and state some implementation
    - splitted state of home page to "launches init" and "launches loaded"
    - fetch method which emits state where launches list is loaded

--- lib/presentation/blocs/home_page/home_page_cubit.dart
+++ lib/presentation/blocs/home_page/home_page_cubit.dart
@@ -1,6 +1,14 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_kepler/domain/repositories/api_repository.dart';
 import 'package:project_kepler/presentation/blocs/home_page/home_page_state.dart';
 
 class HomePageCubit extends Cubit<HomePageState> {
-  HomePageCubit() : super(HomePageState());
+  final ApiRepository repository;
+
+  HomePageCubit(this.repository) : super(LaunchesInit());
+  void fetch() async {
+    await repository
+        .getLaunchList()
+        .then((launches) => emit(LaunchesLoaded(launches)));
+  }
 }
--- lib/presentation/blocs/home_page/home_page_state.dart
+++ lib/presentation/blocs/home_page/home_page_state.dart
@@ -1,1 +1,12 @@
-class HomePageState {}
+import 'package:project_kepler/domain/entities/launch.dart';
+
+abstract class HomePageState {}
+
+class LaunchesInit extends HomePageState{
+
+}
+
+class LaunchesLoaded extends HomePageState {
+  final List<Launch> launches;
+  LaunchesLoaded(this.launches);
+}
