e99fe2c1457e67d9f138b227c3f5f9366115ffe9
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2024-04-03 16:31:41 +0300

    chat now is floating modal:     modal_bottom_sheet replaced with sheet

--- lib/presentation/widgets/chat_wrapper.dart
+++ lib/presentation/widgets/chat_wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:project_kepler/core/extensions/build_context_ext.dart';
+import 'package:sheet/sheet.dart';
+import 'package:sheet/route.dart';
 
 class ChatFAB extends StatelessWidget {
   final Widget child;
@@ -10,18 +11,66 @@ class ChatFAB extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () => showMaterialModalBottomSheet(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-        ),
-        context: context,
-        builder: (context) => Container(
-            padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 20),
-            constraints: BoxConstraints(
-                maxHeight: context.screenHeight - context.screenHeight * 0.20),
-            child: child),
-      ),
+      // onPressed: () => showMaterialModalBottomSheet(
+      //   shape: const RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+      //   ),
+      //   context: context,
+      //   builder: (context) => Container(
+      //       padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 20),
+      //       constraints: BoxConstraints(
+      //           maxHeight: context.screenHeight - context.screenHeight * 0.20),
+      //       child: child),
+      // ),
+      onPressed: () {
+        Navigator.of(context).push(
+          FloatingSheetRoute(
+            builder: (context) {
+              return Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 2, vertical: 20),
+                constraints: BoxConstraints(
+                    maxHeight:
+                        context.screenHeight - context.screenHeight * 0.20),
+                child: child,
+              );
+            },
+          ),
+        );
+      },
       child: const Icon(Icons.auto_awesome),
     );
   }
+}
+
+class FloatingModal extends StatelessWidget {
+  const FloatingModal({super.key, required this.child, this.backgroundColor});
+  final Widget child;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      minimum: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Material(
+        color: backgroundColor,
+        clipBehavior: Clip.antiAlias,
+        borderRadius: BorderRadius.circular(12),
+        child: child,
+      ),
+    );
+  }
+}
+
+class FloatingSheetRoute<T> extends SheetRoute<T> {
+  FloatingSheetRoute({
+    required WidgetBuilder builder,
+  }) : super(
+          builder: (BuildContext context) {
+            return FloatingModal(
+              child: Builder(builder: builder),
+            );
+          },
+          fit: SheetFit.loose,
+        );
 }
