eb09269b70ae427a6193a99ae109e430fa59c04f
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2024-04-12 09:22:33 +0300

    updated favorites work, huge update

--- lib/domain/use_cases/fetch_favourite_launches_use_case.dart
+++ lib/domain/use_cases/fetch_favourite_launches_use_case.dart
@@ -7,28 +7,27 @@ import '../entities/launch.dart';
 
 class FetchFavouriteLaunchesUseCase {
   final FirebaseFirestore firestore;
-  final String userId;
+  String? userId;
   final LaunchDtoToEntityConverter dtoToEntityConverter;
 
   FetchFavouriteLaunchesUseCase({
     required this.firestore,
-    required this.userId,
+    this.userId,
     required this.dtoToEntityConverter,
   });
 
   Future<List<Launch>> call() async {
     final snapshot = await firestore
-        .collection('launches')
-        .where('userId', isEqualTo: userId)
+        .collection('users')
+        .doc(userId)
+        .collection('favorites')
         .get();
-
-    logger.d('Fetched favourite launches for user $userId');
 
     final launches = <Launch>[];
     for (final doc in snapshot.docs) {
       final launchDTO = LaunchDTO.fromJson(doc.data());
       final launch = dtoToEntityConverter.convert(launchDTO);
-      logger.d('Fetched launch ${launch.id}');
+
       final translatedLaunch =
           await languageDetectionService.translateIfNecessary(launch);
       launches.add(translatedLaunch as Launch);
