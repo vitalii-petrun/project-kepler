825c6cd6d0ca2e7c8c47b62172b6da7628949fb0
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2024-04-15 07:49:15 +0300

    added set favourite event use case

---
+++ lib/domain/use_cases/set_favourite_event_use_case.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:project_kepler/data/models/event2_dto.dart';
+import 'package:project_kepler/domain/converters/event_converter.dart';
+import 'package:project_kepler/domain/entities/event.dart';
+
+class SetFavouriteEventUseCase {
+  final FirebaseFirestore firestore;
+  String? userId;
+  final EventEntityToDtoConverter entityToDtoConverter;
+
+  SetFavouriteEventUseCase({
+    required this.firestore,
+    this.userId,
+    required this.entityToDtoConverter,
+  });
+
+  Future<void> call(Event event) async {
+    EventDTO eventDTO = entityToDtoConverter.convert(event);
+    await firestore
+        .collection('users')
+        .doc(userId)
+        .collection('favorites')
+        .doc(userId)
+        .collection('events')
+        .doc(eventDTO.id.toString())
+        .set({
+      'id': eventDTO.id,
+      'name': eventDTO.name,
+    });
+  }
+}
